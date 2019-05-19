42d7d4bbecec079793c3223ea7367a136705a36f
Andrey Naumov <xned81@yandex.ru>
2019-05-19 21:58:56 +0300

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,22 @@
+//1 
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+
+let averageMark = (algebra + geography + physics) / 3;
+
+console.log(`Средний балл ${averageMark}`);
+
+//2
+let name = 'Андрей';
+let message = `Привет, мир! Меня зовут ${name}.`;
+console.log(message);
+
+//3
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
