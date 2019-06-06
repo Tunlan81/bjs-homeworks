9acb46292db49b168a85ad98d2f83dfbd3b3e480
Andrey Naumov <xned81@yandex.ru>
2019-06-07 01:47:45 +0300

    array-proto-hocs correction

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -1,33 +1,23 @@
 //1
 
 function compareArrays(arr1, arr2) {
-    if (arr1.length != arr2.length) return false;
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] != arr2[i]) return false;
-    }
-    return true;
+    return (arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i]));
 }
 
 function memoize(fn, limit) {
     let results = [];
 
-    function calculateAndRemember() {
-        let args = Array.from(arguments).join(', ');
-        existingResult = results.find(result => compareArrays(result.args, args));
+    return function () {
+        existingResult = results.find(result => compareArrays(result.args, Array.from(arguments)));
         if (existingResult) {
             console.log('Результат берется из памяти.');
             return existingResult.result;
         }
-        results.push({args: args, result: fn(...arguments)})
-        if (results.length > limit) {
-            results.splice(0, 1);
-        }
-
+        results.push({args: Array.from(arguments), result: fn(...arguments)})
+        if (results.length > limit) results.shift();
         console.log('Результат берется НЕ из памяти.');
         return results[results.length - 1].result;
     }
-
-    return calculateAndRemember;  
 }
 
 // Check
@@ -42,5 +32,7 @@ console.log(mSum(3, 4));
 console.log(mSum(5, 4));
 
 console.log(mSum(7, 4));
+
+console.log(mSum(3, 4));
 
 console.log(mSum(3, 4));
